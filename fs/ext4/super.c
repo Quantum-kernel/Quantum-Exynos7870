9f5bd18ee19ce137ae58ecc609014626c24e538c
DarkLord1731 <darklord1731@gmail.com>
2018-07-27 18:21:36 +0530

    ext4: Fix ext4_handle_error arguements

--- fs/ext4/super.c
+++ fs/ext4/super.c
@@ -735,7 +735,7 @@ __acquires(bitlock)
 	ext4_handle_error(sb, page_buf);
 	if (page_buf)
 		free_page((unsigned long)page_buf);
-	ext4_commit_super(sb, 1);
+	ext4_handle_error(sb, page_buf);
 	/*
 	 * We only get here in the ERRORS_RO case; relocking the group
 	 * may be dangerous, but nothing bad will happen since the
